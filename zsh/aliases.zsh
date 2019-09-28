c6dd439a7c14e52b1eb2190bb0affdd8fce79915
Daniel Hails <daniel@hails.info>
2019-09-28 17:34:27 +0100

    Add basic zsh aliases

---
+++ zsh/aliases.zsh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+alias reload!='. ~/.zshrc'
+
+alias cls='clear' # Good 'ol Clear Screen command
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias cd..="cd .."
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+alias :q="exit"
+alias q="exit"
+alias ch="history -c && > ~/.bash_history"
+alias path='printf "%b\n" "${PATH//:/\\n}"'
+alias ll="ls -l"
+alias la="ls -la"
+alias m="man"
+
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
