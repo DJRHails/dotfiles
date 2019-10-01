e4b55ebd866b56d9d00fb90c7b7a13db30f91885
Daniel Hails <daniel@hails.info>
2019-10-01 23:55:52 +0100

    Add todos for even more installation

--- zsh/install.macos.sh
+++ zsh/install.macos.sh
@@ -23,3 +23,9 @@ fi
 # Install fira code
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
+
+# TODO: Install hyper
+# TODO: brew cask install hyper
+# TODO: > hyper i hyper-snazzy
+
+# REFER TO: https://github.com/StefanScherer/dotfiles
