9f3173808beeec1cd1db47a992d1ce7085c34827
Daniel Hails <daniel@hails.info>
2019-10-04 21:05:10 +0100

    Add install prompt for zplug install

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -15,6 +15,7 @@ zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
+   printf "Install? [y/N]: "
    if read -q; then
        echo; zplug install
    fi
