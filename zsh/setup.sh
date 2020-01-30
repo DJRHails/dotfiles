b749802327f4d38e8296949b3cc9de368cb9f88a
Daniel Hails <daniel@hails.info>
2020-01-30 15:42:51 +0000

    Support sites jump to as well as project jump to

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -16,7 +16,11 @@ setup_zshrc () {
     local project_dir="$(feedback::get_answer)"
     project_dir=${project_dir:-"$HOME/projects"}
 
-    sed -e "s+PROJECT_ROOT+$project_dir+g" \
+    feedback::ask " - Where are you going to store your sites? ($HOME/sites)"
+    local sites_dir="$(feedback::get_answer)"
+    sites_dir=${sites_dir:-"$HOME/sites"}
+
+    sed -e "s+PROJECT_ROOT+$project_dir+g;s+SITES_ROOT+$sites_dir+g" \
       $LOCAL_ZSHRC.example > $LOCAL_ZSHRC
 
     log::result $? 'generated zsh/zshrc'
