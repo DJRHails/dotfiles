6c10996c4be43aa816d3fd92637716d2592ef35a
Daniel Hails <daniel@hails.info>
2020-05-17 19:32:20 +0100

    Setup Rust install conditionally

--- rust/install.sh
+++ rust/install.sh
@@ -1,2 +1,9 @@
-# Install 
-curl https://sh.rustup.rs -sSf | sh
+. "$DOTFILES/scripts/core/main.sh"
+
+# Install
+# Check version exisiting
+if ! platform::command_exists "cargo"
+then
+  curl https://sh.rustup.rs -sSf | sh
+  log::result $? "Install rust, cargo"
+fi
