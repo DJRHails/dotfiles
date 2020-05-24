f622b1c63dc27621a56d0411ccaaa77676ed7251
Daniel Hails <daniel@hails.info>
2020-05-24 13:21:33 +0100

    Make ssh config local so can append IdentifyFile paths

---
+++ ssh/setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+setup_ssh_config () {
+  local LOCAL_CONFIG=ssh/config
+  if ! [ -f $LOCAL_CONFIG ]
+  then
+    cat \
+      $LOCAL_CONFIG.tmpl > $LOCAL_CONFIG
+
+    log::result $? 'generated .ssh/config'
+  fi
+}
+
+. "$DOTFILES/scripts/core/main.sh"
+setup_ssh_config
