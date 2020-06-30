993fd06150dd418dc274bcede9186faf327fd57c
Daniel Hails <daniel@hails.info>
2020-06-30 17:58:01 +0100

    Add flutter module

---
+++ flutter/install.linux.sh
@@ -0,0 +1,9 @@
+. "$DOTFILES/scripts/core/main.sh"
+
+open "https://flutter.dev/docs/get-started/install/linux"
+open "https://developer.android.com/studio"
+
+# tar -xf
+# mv flutter /opt/flutter
+# export PATH="$PATH:/opt/flutter/bin"
+# flutter doctor
---
+++ flutter/path.zsh
@@ -0,0 +1,5 @@
+export PATH="$PATH:/opt/flutter/bin"
+export PATH="$PATH:/opt/android-studio/bin"
+export PATH="$PATH:/opt/Android/Sdk/"
+export PATH="$PATH:/opt/Android/Sdk/platform-tools"
+export ANDROID_SDK_ROOT="/opt/Android/Sdk/"
