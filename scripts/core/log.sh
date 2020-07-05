e675b653d92a4e0558cc41e1b827544028ac04b4
Daniel Hails <daniel@hails.info>
2020-07-05 19:51:23 +0100

    Add spinner, logging exec

--- scripts/core/log.sh
+++ scripts/core/log.sh
@@ -2,6 +2,78 @@ log::error() {
     log::red "   [✖] $1 $2\n"
 }
 
+log::spinner() {
+  local -r FRAMES='⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏'
+  local -r NUMBER_OR_FRAMES=${#FRAMES}
+  local -r INTERVAL=0.08
+  local -r PID="$1"
+  local -r CMDS="$2"
+  local -r MSG="$3"
+
+  local i=0
+  local frameText=""
+
+  # Provide more space so that the text hopefully
+  # doesn't reach the bottom line of the terminal window.
+  #
+  # This is a workaround for escape sequences not tracking
+  # the buffer position (accounting for scrolling).
+  #
+  # See also: https://unix.stackexchange.com/a/278888
+
+  printf "\n\n\n"
+  tput cuu 3
+  tput sc
+
+  while kill -0 "$PID" &>/dev/null; do
+    frameText="   [${FRAMES:i++%NUMBER_OR_FRAMES:1}] $MSG"
+    printf "%s\n" "$frameText"
+    sleep $INTERVAL
+    tput rc
+  done
+}
+
+set_trap() {
+  trap -p "$1" | grep "$2" &> /dev/null \
+    || trap '$2' "$1"
+}
+
+kill_all_subprocesses() {
+  local i=""
+  for i in $(jobs -p); do
+    kill "$i"
+    wait "$i" &> /dev/null
+  done
+}
+
+log::execute() {
+  local -r CMDS="$1"
+  local -r MSG="${2:-$1}"
+  local -r TMP_FILE="$(mktemp /tmp/err_XXXXX)"
+
+  local exitCode=0
+  local cmdsPID=""
+
+  set_trap "EXIT" "kill_all_subprocesses"
+
+  eval "$CMDS" &> /dev/null 2> "$TMP_FILE" &
+  cmdsPID=$!
+
+  log::spinner "$cmdsPID" "$CMDS" "$MSG"
+
+  wait "$cmdsPID" &> /dev/null
+  exitCode=$?
+
+  log::result $exitCode "$MSG"
+  if [ $exitCode -ne 0 ]; then
+    log::error_stream < "$TMP_FILE"
+  fi
+
+  rm -rf "$TMP_FILE"
+
+  return $exitCode
+}
+
 log::error_stream() {
     while read -r line; do
         log::error "↳ ERROR: $line"
