5ea02cb84a7a26497fef930da2a5c1835c309651
Daniel Hails <daniel@hails.info>
2020-07-05 19:50:45 +0100

    Add install script utilities

---
+++ scripts/core/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+install::with() {
+  local -r PACKAGE_READABLE_NAME="$1"
+  local -r PACKAGE="$2"
+  local -r EXTRA_ARGUMENTS="$3"
+  local -r PACKAGE_MANAGER="$4"
+
+  if ! platform::command_exists "$PACKAGE"; then
+    log::execute "$PACKAGE_MANAGER install $EXTRA_ARGUMENTS $PACKAGE" "$PACKAGE_READABLE_NAME"
+  else
+    log::success "$PACKAGE_READABLE_NAME"
+  fi
+}
+
+install::package() {
+  install::with $1 $2 $3 "$(platform::main_package_manager)"
+}
+
+install::snap() {
+  install::with $1 $2 $3 "sudo snap"
+}
+
+install::cask() {
+  install::with $1 $2 $3 "brew cask"
+}
--- scripts/core/main.sh
+++ scripts/core/main.sh
@@ -2,19 +2,12 @@
 
 if ! ${DOT_MAIN_SOURCED:-false} ; then
    source "${DOTFILES}/scripts/core/platform.sh"
-   # source "${DOTFILES}/scripts/core/osx_gnu.sh"
-   # if ! ${DOT_EXPORTED_PATH:-false}; then
-   #    if ! platform::command_exists dot; then
-   #       export PATH="${DOTFILES}/bin/:${PATH}"
-   #       export DOT_EXPORTED_PATH=true
-   #    fi
-   # fi
-   # source "${DOTFILES}/scripts/core/collections.sh"
    source "${DOTFILES}/scripts/core/log.sh"
-   # source "${DOTFILES}/scripts/core/documentation.sh"
    source "${DOTFILES}/scripts/core/filesystem.sh"
-   # source "${DOTFILES}/scripts/core/string.sh"
    source "${DOTFILES}/scripts/core/feedback.sh"
+
+   # Requires platform, log
+   source "${DOTFILES}/scripts/core/install.sh"
    readonly DOT_MAIN_SOURCED=true
 fi
 
