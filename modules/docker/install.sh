b34dfc21310a75cb0a4f6e7513a9553398f30d6e
DJRHails <daniel@hails.info>
2022-03-17 01:05:27 -0400

    Add better docker install

--- modules/docker/install.sh
+++
@@ -1,4 +0,0 @@
-# Setup repo, depends on os: https://docs.docker.com/engine/install
-# Install Docker engine: `docker-ce docker-ce-cli containerd.io`
-# sudo systemctl start docker
-# sudo usermod -aG docker `whoami`
