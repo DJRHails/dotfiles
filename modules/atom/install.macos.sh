b36eccc4f07db7231776dd7bc323406b1178390f
DJRHails <daniel@hails.info>
2023-01-12 16:13:28 +0000

    Remove atom module as unused

--- modules/atom/install.macos.sh
+++
@@ -1,5 +0,0 @@
-. "$DOTFILES/scripts/core/main.sh"
-
-install::cask "Atom" "atom"
-apm install package-sync
-#! Then open Atom and use menu Packages / Package Sync / Sync to install all other plugins.
