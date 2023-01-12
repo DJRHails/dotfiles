33b32a371a5cfe5cbd03a17420758b2d9bcb33d6
DJRHails <daniel@hails.info>
2023-01-12 16:14:58 +0000

    Improve docs helper to work in more edgecases

--- scripts/core/doc.sh
+++ scripts/core/doc.sh
@@ -1,12 +1,19 @@
+##? This is a mistake! You're looking at help for the doc.sh file
+
 doc::help_msg() {
    local -r file="$1"
+   echo "USAGE: $(basename "$file") [options]"
    grep "^##?" "$file" | cut -c 5-
 }
 
 doc::maybe_help() {
-   local -r file="$0"
+   local -r file="${0}"
 
-   case "${!#:-}" in
-      -h|--help|--version) doc::help_msg "$file"; exit 0 ;;
-   esac
+   for arg in "${@}"; do
+      case "${arg}" in
+         -h|--help|--version) doc::help_msg "$file"; return 1;;
+      esac
+   done
+
+   return 0;
 }
