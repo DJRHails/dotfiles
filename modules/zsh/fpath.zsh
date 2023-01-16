d761885f7923566a75d3b9c39817e883fb367eda
DJRHails <daniel@hails.info>
2023-01-16 15:03:46 +0000

    Remove $ZSH override

--- modules/zsh/fpath.zsh
+++ modules/zsh/fpath.zsh
@@ -1,5 +1,5 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($ZSH/modules/*) do;
+for topic_folder ($DOTFILES/modules/*) do;
     if [ -d $topic_folder ]; then
         fpath=($topic_folder $fpath);
     fi;
