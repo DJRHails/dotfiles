2b90c0ca3d00e759baa89acbef968e207944ec59
DJRHails <daniel@hails.info>
2023-04-06 18:02:54 +0100

    Add aenv file

--- modules/python/aliases.zsh
+++ modules/python/aliases.zsh
@@ -4,6 +4,15 @@ alias venv='python3 -m venv .venv && source .venv/bin/activate && pip install --
 alias ae='source .venv/bin/activate'
 alias de='deactivate'
 
+function aenv() {
+  local env_file=${1:-'.env'}
+  if [ -f $env_file ]; then
+    export $(echo $(cat $env_file | sed 's/#.*//g'| xargs) | envsubst)
+  else
+    echo "File '$env_file' not found"
+  fi
+}
+
 function poetry() {
   # if POETRY_DONT_LOAD_ENV is *not* set, then load .env if it exists
   if [[ -z "$POETRY_DONT_LOAD_ENV" && -f .env ]]; then
