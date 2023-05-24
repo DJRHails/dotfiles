ff0fce7a80edfbeaa2f08a84d6cc490aac413de7
DJRHails <daniel@hails.info>
2023-05-24 17:49:54 +0100

    Port is more memorable than server

--- modules/zsh/aliases.zsh
+++ modules/zsh/aliases.zsh
@@ -161,15 +161,15 @@ fkill() {
   fi
 }
 
-# mnemonic: [F]uzzy [Kill] [S]erver
+# mnemonic: [F]uzzy [Kill] [P]ort
 # show output of "lsof -Pwni tcp", use [tab] to select one or multiple entries
-fkills() {
+fkillport() {
   local pid=$(lsof -Pwni tcp | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:tcp]'" | awk '{print $2}')
 
   if [ "x$pid" != "x" ]
   then
     echo $pid | xargs kill -${1:-9}
-    fkills
+    fkillport
   fi
 }
 
