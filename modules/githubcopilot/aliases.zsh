345b301df50e14da7e126eb6d2bf94a604773691
DJRHails <hello@hails.info>
2023-11-10 10:42:49 +0000

    Add better copilot aliases

--- modules/githubcopilot/aliases.zsh
+++ modules/githubcopilot/aliases.zsh
@@ -1,19 +1,7 @@
 # echo "$(github-copilot-cli alias -- "$0")"
 
-copilot_what-the-shell() {
-    TMPFILE=$(mktemp)
-    trap 'rm -f $TMPFILE' EXIT
-    if /usr/bin/github-copilot-cli what-the-shell "$@" --shellout $TMPFILE; then
-        if [ -e "$TMPFILE" ]; then
-            FIXED_CMD=$(cat $TMPFILE)
-            print -s "$FIXED_CMD"
-            eval "$FIXED_CMD"
-        else
-            echo "Apologies! Extracting command failed"
-        fi
-    else
-        return 1
-    fi
-}
+# gh auth login --web -h github.com
+# gh extension install github/gh-copilot --force
 
-alias '??'='copilot_what-the-shell'
+alias '??'='gh copilot suggest'
+alias 'wtf'='gh copilot explain'
