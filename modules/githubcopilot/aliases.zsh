12ac33af468e52c90e837143e599be6efb703b83
DJRHails <hello@hails.info>
2024-04-19 12:48:06 +0100

    Use askllm over copilot

--- modules/githubcopilot/aliases.zsh
+++
@@ -1,7 +0,0 @@
-# echo "$(github-copilot-cli alias -- "$0")"
-
-# gh auth login --web -h github.com
-# gh extension install github/gh-copilot --force
-
-alias '??'='gh copilot suggest'
-alias 'wtf'='gh copilot explain'
--- modules/githubcopilot/install.sh
+++
@@ -1,7 +0,0 @@
-# https://www.npmjs.com/package/@githubnext/github-copilot-cli
-
-
-sudo npm install -g @githubnext/github-copilot-cli
-github-copilot-cli auth
-
-# Usage: ?? list all files
