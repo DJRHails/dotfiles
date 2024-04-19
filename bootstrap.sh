d55e02946c272c4c895b30e8b24d123a9621a085
DJRHails <hello@hails.info>
2024-04-19 13:10:55 +0000

    Add piknik install

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,7 +33,7 @@ parse_args() {
             allModules=true
           ;;
           -c|--cli)
-            scanned_valid_modules+=("$DOTFILES/modules/zsh" "$DOTFILES/modules/ssh" "$DOTFILES/modules/git")
+            scanned_valid_modules+=("$DOTFILES/modules/zsh" "$DOTFILES/modules/ssh" "$DOTFILES/modules/git" "$DOTFILES/modules/piknik")
           ;;
           *)
             scanned_valid_modules+=("$DOTFILES/modules/$1")
---
+++ modules/piknik/install.ubuntu.sh
@@ -0,0 +1,7 @@
+. "$DOTFILES/scripts/core/main.sh"
+
+wget https://github.com/jedisct1/piknik/releases/download/0.10.1/piknik-linux_x86_64-0.10.1.tar.gz
+tar -xvf piknik-linux_x86_64-0.10.1.tar.gz
+sudo mv linux-x86_64/piknik /usr/local/bin
+rm -rf piknik-linux_x86_64-0.10.1.tar.gz linux-x86_64
+rm -rf linux-x86_64
