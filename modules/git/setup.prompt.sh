3acd67348dcc18cd69974540b0b7ac9d3c302666
DJRHails <hello@hails.info>
2026-02-11 12:44:36 +0000

    Add defaults for git author prompts
    Auto-uses defaults in non-interactive mode (--cli), shows them
    in brackets for interactive mode.

--- modules/git/setup.prompt.sh
+++ modules/git/setup.prompt.sh
@@ -2,15 +2,27 @@
 
 prompt::author_name() {
   if [[ -z $GIT_AUTHOR_NAME ]]; then
-    feedback::ask ' - What is your github author username?'
-    GIT_AUTHOR_NAME=$(feedback::get_answer)
+    local default="DJRHails"
+    if [ "$skipQuestions" = true ]; then
+      GIT_AUTHOR_NAME="$default"
+    else
+      feedback::ask " - What is your github author username? [$default]"
+      GIT_AUTHOR_NAME=$(feedback::get_answer)
+      GIT_AUTHOR_NAME="${GIT_AUTHOR_NAME:-$default}"
+    fi
   fi
 }
 
 prompt::author_email() {
   if [[ -z $GIT_AUTHOR_EMAIL ]]; then
-    feedback::ask ' - What is your github author email?'
-    GIT_AUTHOR_EMAIL=$(feedback::get_answer)
+    local default="hello@hails.info"
+    if [ "$skipQuestions" = true ]; then
+      GIT_AUTHOR_EMAIL="$default"
+    else
+      feedback::ask " - What is your github author email? [$default]"
+      GIT_AUTHOR_EMAIL=$(feedback::get_answer)
+      GIT_AUTHOR_EMAIL="${GIT_AUTHOR_EMAIL:-$default}"
+    fi
   fi
 }
 
